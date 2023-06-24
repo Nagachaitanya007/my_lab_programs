a01a9fd6cc187c337661d14f23ed5434e17cc1a0
Nagachaitanya007 <109469278+Nagachaitanya007@users.noreply.github.com>
2023-06-24 10:31:41 +0530

    Update 14.py

--- lab_programs/14.py
+++ lab_programs/14.py
@@ -25,3 +25,30 @@ if found:
     print(f"Element {target} found at index {index}.")
 else:
     print("Element not found in the array.")
+
+''' def linear_search(list, element):
+  for i in range(len(list)):
+    if list[i] == element:
+      return i
+  return -1
+
+
+def main():
+  list = []
+  n = int(input("Enter the number of elements in the list: "))
+  for i in range(n):
+    element = int(input("Enter element number {}: ".format(i + 1)))
+    list.append(element)
+
+  element = int(input("Enter the element to search for: "))
+  index = linear_search(list, element)
+
+  if index == -1:
+    print("Element not found")
+  else:
+    print("Element found at index:", index)
+
+
+if __name__ == "__main__":
+  main()
+ '''
